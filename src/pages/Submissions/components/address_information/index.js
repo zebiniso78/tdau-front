693f7776f283e382ad440a52c501c8ea0f401461
Shahbozbek2000 <tuitstudent2000@gmail.com>
2022-04-08 15:14:51 +0500

    address info complete

--- src/pages/Submissions/components/address_information/index.js
+++ src/pages/Submissions/components/address_information/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Title } from '../../../../styles/globalStyle'
-import { AddressInfoProvider, AddressForm, Paragraph, CheckboxWrapper, MaskInputWrapper } from "./style"
+import {
+  AddressInfoProvider,
+  AddressForm,
+  Paragraph,
+  CheckboxWrapper,
+  MaskInputWrapper,
+  AddressFooter
+} from "./style"
 import { useForm, Controller } from "react-hook-form"
 import { CheckboxComponent } from '../../../../components/checkbox'
 import { EntityForm } from './entityForm'
 import { PhoneMask } from 'components/mask'
+import { InputComponent } from 'components/input/controllerInput'
+import { Link } from 'react-router-dom'
 
 
 export function AddressInformation() {
@@ -58,7 +67,65 @@ export function AddressInformation() {
             required={true}
             validators={['required', 'isNumber']} />
         </MaskInputWrapper>
-
+        <Paragraph className='mt-2'>Большая часть нашей корреспонденции будет вестись по электронной почте,
+          но если мы отправим вам что-либо по почте, мы будем использовать адрес,
+          указанный вами в этом разделе. Если ваш адрес для корреспонденции является
+          временным, сообщите нам дату, когда вы въехали и когда собираетесь уехать.
+          Это поможет нам убедиться, что мы всегда отправляем корреспонденцию по
+          правильному адресу.</Paragraph>
+        <MaskInputWrapper>
+          <PhoneMask
+            Controller={Controller}
+            control={control}
+            nameProps='personal_phone'
+            title='Личный адрес элек.почты заявителя'
+            required={true}
+            validators={['required', 'isNumber']} />
+        </MaskInputWrapper>
+        <AddressFooter>
+          <Title className='mt-4 form-title'>Адрес электронной почты</Title>
+          <Paragraph className='mt-3'>Университет отправляет корреспонденцию по электронной почте,
+            поэтому, пожалуйста, убедитесь, что ваш адрес электронной почты верен
+            и что он будет действителен в течение всего процесса подачи заявки.
+            Если вы являетесь зарубежным представителем, заполняющим эту форму
+            от имени заявителя, укажите в этом разделе личный адрес электронной
+            почты заявителя. Мы собрали контактные данные вашего отделения в другом
+            месте приложения, и агентство будет продолжать получать корреспонденцию.</Paragraph>
+          <div className='row'>
+            <div className='col-lg-5 col-md-6 col-sm-6 col-12'>
+              <InputComponent
+                Controller={Controller}
+                control={control}
+                nameProps="index"
+                plProps="Введите Номер элек.почты"
+                label='Личный адрес элек.почты заявителя*'
+              />
+            </div>
+            <div className='col-lg-5 col-md-6 col-sm-6 col-12'>
+              <InputComponent
+                Controller={Controller}
+                control={control}
+                nameProps="email"
+                plProps="Введите Номер элек.почты"
+                label='Подтвердите адрес электронной почты'
+              />
+            </div>
+            <div className='col-lg-2' />
+            <div className='col-lg-5 col-md-6 col-sm-6 col-12'>
+              <Paragraph className='footer-paragraph'>
+                Университет отправляет большую часть корреспонденции
+                по электронной почте, поэтому убедитесь, что ваш адрес
+                электронной почты верен и является действительным в течение
+                всего процесса подачи заявки.
+              </Paragraph>
+            </div>
+            <div className='col-lg-5 col-md-6 col-sm-6 col-12'>
+              <Paragraph>
+                <Link to='/address-info' className='nav-link'>Отправить СМС ещё раз</Link>
+              </Paragraph>
+            </div>
+          </div>
+        </AddressFooter>
       </AddressForm>
     </AddressInfoProvider>
   )
