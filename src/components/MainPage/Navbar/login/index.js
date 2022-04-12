a5c55385249372d08809436a45b9e69dd3701017
Asadbek <azamov980@gmail.com>
2022-04-12 23:43:55 +0500

    modal styles

--- src/components/MainPage/Navbar/login/index.js
+++ src/components/MainPage/Navbar/login/index.js
@@ -1,87 +1,81 @@
 import { PhoneMask } from 'components/mask';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import PureModal from 'react-pure-modal';
-import Button from "../../../button"
+import Button from '../../../button';
 import { useForm, Controller } from 'react-hook-form';
 import { authApi } from 'services/api/pagesApi';
 import { InputComponent } from 'components/input/controllerInput';
 import { useHistory } from 'react-router-dom';
 
-
-
 export function Login({ setModal, setRegisterModel, modal }) {
-   const [isLoading, setIsLoading] = useState(false)
-   const history = useHistory()
-   const {
-      handleSubmit,
-      watch,
-      reset,
-      control,
-      formState: { errors },
-   } = useForm();
+  const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
+  const {
+    handleSubmit,
+    watch,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm();
 
-   const onSubmit = async (data) => {
-      try {
-         setIsLoading(true)
-         let formData = new FormData()
-         formData.append('phone', `+998${data?.phone}`)
-         formData.append('password', data?.password)
-         let res = await authApi.login(formData)
-         localStorage.setItem('token', res?.token)
-         setIsLoading(false)
-         setModal(false)
-         history.push('/')
-      } catch (e) {
-         console.log(e)
-         setIsLoading(false)
-      }
-   }
-   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-         <PureModal
-            header="Вход"
-            footer={
-               <div className="footer-button__wrapper">
-                  <Button
-                     type="submit"
-                     title="Вход"
-                     disabled={isLoading}
-                  />
-                  <Button
-                     type="submit"
-                     title="Зарегистрироваться"
-                     bgColor="transparent"
-                     color="#2e7df6"
-                     onClick={() => setRegisterModel(true)}
-                  />
-               </div>
-            }
-            isOpen={modal}
-            closeButton="X"
-            closeButtonPosition="header"
-            onClose={() => {
-               setModal(false);
-               return true;
-            }}
-         >
-            <PhoneMask
-               Controller={Controller}
-               control={control}
-               nameProps='phone'
-               title='Phone number'
-               required={true}
-               validators={['required', 'isNumber']} />
-            <InputComponent
-               Controller={Controller}
-               control={control}
-               nameProps="password"
-               plProps="Enter password"
-               label='Password*'
-               required={true}
-               className='registration-input'
-               type='password'
+  const onSubmit = async (data) => {
+    try {
+      setIsLoading(true);
+      let formData = new FormData();
+      formData.append('phone', `+998${data?.phone}`);
+      formData.append('password', data?.password);
+      let res = await authApi.login(formData);
+      localStorage.setItem('token', res?.token);
+      setIsLoading(false);
+      setModal(false);
+      history.push('/');
+    } catch (e) {
+      console.log(e);
+      setIsLoading(false);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <PureModal
+        header="Вход"
+        footer={
+          <div className="footer-button__wrapper">
+            <Button type="submit" title="Вход" disabled={isLoading} />
+            <Button
+              type="submit"
+              title="Зарегистрироваться"
+              bgColor="transparent"
+              color="#2e7df6"
+              onClick={() => setRegisterModel(true)}
             />
-         </PureModal>
-      </form>
-   )
+          </div>
+        }
+        isOpen={modal}
+        closeButtonPosition="header"
+        onClose={() => {
+          setModal(false);
+          return true;
+        }}
+      >
+        <PhoneMask
+          Controller={Controller}
+          control={control}
+          nameProps="phone"
+          title="Phone number"
+          required={true}
+          validators={['required', 'isNumber']}
+        />
+        <InputComponent
+          Controller={Controller}
+          control={control}
+          nameProps="password"
+          plProps="Enter password"
+          label="Password*"
+          required={true}
+          className="registration-input"
+          type="password"
+        />
+      </PureModal>
+    </form>
+  );
 }
--- src/components/MainPage/Navbar/verify/index.js
+++ src/components/MainPage/Navbar/verify/index.js
@@ -1,89 +1,92 @@
 import { CustomMask } from 'components/mask/customMask';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import PureModal from 'react-pure-modal';
 import { useHistory } from 'react-router-dom';
 import { authApi } from 'services/api/pagesApi';
-import Button from "../../../button"
+import Button from '../../../button';
 
+export function Verify({
+  setConfirmModel,
+  setRegisterModel,
+  confirmModel,
+  phoneNumber,
+}) {
+  const {
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm();
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
+  async function onSubmit(data) {
+    try {
+      setIsLoading(true);
+      let formData = new FormData();
+      formData.append('phone', phoneNumber);
+      formData.append('code', data?.code);
+      let res = await authApi.verify(formData);
+      localStorage.setItem('token', res?.token);
+      setIsLoading(false);
+      setConfirmModel(false);
+      history.push('/');
+    } catch (e) {
+      console.log(e);
+      setIsLoading(false);
+    }
+  }
 
-
-export function Verify({ setConfirmModel, setRegisterModel, confirmModel, phoneNumber }) {
-   const {
-      handleSubmit,
-      watch,
-      control,
-      formState: { errors },
-   } = useForm();
-   const history = useHistory()
-   const [isLoading, setIsLoading] = useState(false)
-   async function onSubmit(data) {
-      try {
-         setIsLoading(true)
-         let formData = new FormData()
-         formData.append("phone", phoneNumber)
-         formData.append("code", data?.code)
-         let res = await authApi.verify(formData)
-         localStorage.setItem('token', res?.token)
-         setIsLoading(false)
-         setConfirmModel(false)
-         history.push('/')
-      } catch (e) {
-         console.log(e)
-         setIsLoading(false)
-      }
-   }
-
- 
-   return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-         <PureModal
-            header="Вход"
-            footer={
-               <div className="footer-button__wrapper">
-                  <Button
-                     type="submit"
-                     title="Зарегистрироваться"
-                     disabled={isLoading} />
-                  <Button
-                     type="submit"
-                     title="Выход"
-                     bgColor="transparent"
-                     color="#2e7df6"
-                  // onClick={() => {
-                  //    setConfirmModel(false);
-                  //    setRegisterModel(false);
-                  // }}
-                  />
-               </div>
-            }
-            isOpen={confirmModel}
-            closeButton="X"
-            closeButtonPosition="header"
-            onClose={() => {
-               setConfirmModel(false);
-               return true;
-            }}
-         >
-            {/* <Input
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <PureModal
+        header="Вход"
+        footer={
+          <div className="footer-button__wrapper">
+            <Button
+              type="submit"
+              title="Зарегистрироваться"
+              disabled={isLoading}
+            />
+            <Button
+              type="submit"
+              title="Выход"
+              bgColor="transparent"
+              color="#2e7df6"
+              // onClick={() => {
+              //    setConfirmModel(false);
+              //    setRegisterModel(false);
+              // }}
+            />
+          </div>
+        }
+        isOpen={confirmModel}
+        // closeButton="X"
+        closeButtonPosition="header"
+        onClose={() => {
+          setConfirmModel(false);
+          return true;
+        }}
+      >
+        {/* <Input
                type={`password`}
                required
                title="СМС-код"
                placeholder="Введите код"
                length={80}
             /> */}
-            <CustomMask
-               Controller={Controller}
-               control={control}
-               nameProps="code"
-               mask="1111"
-               title='СМС-код'
-               placeholder='----'
-            />
-            <p>
-               <span>00:56</span>Отправить код ещё раз
-            </p>
-         </PureModal>
-      </form>
-   )
+        <CustomMask
+          Controller={Controller}
+          control={control}
+          nameProps="code"
+          mask="1111"
+          title="СМС-код"
+          placeholder="----"
+        />
+        <p>
+          <span>00:56</span>Отправить код ещё раз
+        </p>
+      </PureModal>
+    </form>
+  );
 }
