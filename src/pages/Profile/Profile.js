b872fe89c35aa3ba44cd8cfcc64f382f71c9f248
Asadbek <azamov980@gmail.com>
2022-04-20 11:18:48 +0500

    last

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
-import Navbar from '../../components/MainPage/Navbar/Navbar';
-import Footer from '../../components/MainPage/Footer/Footer';
 import { ProfileSection, ProfileBody, ProfilePaper } from './ProfileStyle';
-import PersonalCard from '../../components/PersonalCard';
-import InputsCard from '../../components/InputsCard';
 import { Container } from '../../common/grid';
 import SupportComponent from './support';
-
+import { Alert, Button } from 'antd';
 function Profile() {
-  const [support, setSupport] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <>
       <ProfileSection>
@@ -16,23 +12,89 @@ function Profile() {
         <ProfileBody>
           <Container className="profile-container">
             <ProfilePaper>
-              <div class="alert alert-primary" role="alert">
-                <h4 class="alert-heading">Your admission has been sent!</h4>
-                <p>Your admissions will be reviewed and replied soon</p>
-                <hr />
-                <p class="mb-0">
-                  If your request is left unanswered for a long time, please
-                  contact our{' '}
-                  <a
-                    onClick={() => setSupport(!support)}
-                    href="#"
-                    class="alert-link"
+              <Alert
+                message="Your admission has been sent!"
+                description="Your admissions will be reviewed and replied soon!   If your request is left unanswered for a long time, please
+                contact our support center."
+                type="info"
+                showIcon
+                banner
+                action={
+                  <Button
+                    onClick={() => {
+                      setIsModalVisible(true);
+                    }}
+                    size="small"
+                    type="warning"
                   >
-                    support center.
-                  </a>
-                </p>
+                    Support center
+                  </Button>
+                }
+              />
+
+              {/* Accepted  */}
+
+              <div className="row align-items-center">
+                <div className="col-6 col-md-8 col-lg-9">
+                  <Alert
+                    message="Your admission accepted!"
+                    description="You have been successfully admitted to the student body! you have been provided with a login and password to log in!"
+                    type="success"
+                    showIcon
+                    banner
+                  />
+                </div>
+                <div className="col-6 col-md-4 col-lg-3 ">
+                  <Alert
+                    message={`Login : login `}
+                    description={`Password : password `}
+                    type="info"
+                    banner
+                    showIcon={false}
+                  />
+                </div>
               </div>
-              {support ? <SupportComponent /> : null}
+
+              {/* Accepted end */}
+
+              {/* Rejected  */}
+              <div className="row align-items-center">
+                <div className="col-6 col-md-8">
+                  <Alert
+                    message="Your admission rejected!"
+                    description="You can write to our support center"
+                    type="error"
+                    showIcon
+                    banner
+                    action={
+                      <Button
+                        onClick={() => {
+                          setIsModalVisible(true);
+                        }}
+                        size="small"
+                        danger
+                      >
+                        Support center
+                      </Button>
+                    }
+                  />
+                </div>
+                <div className="col-6 col-md-4">
+                  <Alert
+                    message={`Comment:`}
+                    description={`Your DTM card is incomplete!`}
+                    type="error"
+                    banner
+                    showIcon={false}
+                  />
+                </div>
+              </div>
+
+              {/* Rejected end */}
+              <SupportComponent
+                isModalVisible={isModalVisible}
+                setIsModalVisible={setIsModalVisible}
+              />
             </ProfilePaper>
 
             {/* <div className="user-card">
--- src/pages/Profile/support/index.js
+++ src/pages/Profile/support/index.js
@@ -1,26 +1,34 @@
 import { NextBtnComponent } from 'components/buttons/next-btn';
 import React from 'react';
+import { Modal } from 'antd';
 import { Container } from './style';
+export default function SupportComponent({
+  setIsModalVisible,
+  isModalVisible,
+}) {
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-export default function SupportComponent() {
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
-    <Container>
-      <p>How can we help?</p>
+    <Modal
+      title="Basic Modal"
+      visible={isModalVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Container>
+        <p className="px-2 py-1 bg-light mb-3">How can we help?</p>
 
-      <div className="row align-items-end justify-content-between">
-        <div className="col-md-8">
-          <textarea placeholder="your message"></textarea>
-        </div>
-        <div className="col-md-4">
-          <div>
-            <NextBtnComponent
-              name="Отравить"
-              className="next-btn"
-              type="submit"
-            />
+        <div className="row align-items-end justify-content-between">
+          <div className="col-md-8">
+            <textarea placeholder="your message"></textarea>
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </Modal>
   );
 }
