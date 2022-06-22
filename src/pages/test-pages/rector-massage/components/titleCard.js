1fda906f2bc7c3734898e5bb1d9ce5a63262be0b
Asadbek <azamov980@gmail.com>
2022-06-22 22:02:27 +0500

    last edited rextor message tugadi

--- src/pages/test-pages/rector-massage/components/titleCard.js
+++ src/pages/test-pages/rector-massage/components/titleCard.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import { Container, ContentCard, ContentParagraf, H1, Position } from '../container/style';
+import {
+  Container,
+  ContentCard,
+  ContentParagraf,
+  H1,
+  Position,
+} from '../container/style';
 import rectorImg from 'assets/rectors/rectorPhoto.png';
-import QuoteIcon from 'assets/rectors/QuoteIcon.png'
+import QuoteIcon from 'assets/rectors/QuoteIcon.png';
 
-function TitleCard() {
-    return (
-        <ContentCard>
-            <div className="left-img">
-                <div className="cubick-rub"> </div>
-                <img src={rectorImg} alt="Rector img" />
-            </div>
-            <div className='card-container'>
-                <div className="right-content">
-                    <H1>Khodzhaev Jamshid Abdukhakimovich</H1>
-                    <Position>Rector</Position>
-                    <ContentParagraf>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                        but also the leap into electronic typesetting, remaining essentially unchanged.
-                    </ContentParagraf>
-                </div>
-                <img className="card-img" src={QuoteIcon} alt="quote img" />
-            </div>
-        </ContentCard>
-    );
+function TitleCard({ desc, rector_photo, rector_name }) {
+  return (
+    <ContentCard>
+      <div className="left-img">
+        <div className="cubick-rub"> </div>
+        <img
+          src={`${process.env.REACT_APP_API_SECOND_ROOT}/${rector_photo}`}
+          alt="Rector img"
+        />
+      </div>
+      <div className="card-container">
+        <div className="right-content">
+          <H1>{rector_name}</H1>
+          <Position>Rector</Position>
+          <ContentParagraf>{desc}</ContentParagraf>
+        </div>
+        <img className="card-img" src={QuoteIcon} alt="quote img" />
+      </div>
+    </ContentCard>
+  );
 }
 
 export default TitleCard;
--- src/services/api/pagesApi.js
+++ src/services/api/pagesApi.js
@@ -1,17 +1,13 @@
-
 import Auth from '../api/modules/auth';
 import Admissions from './modules/admissions';
 import News from './modules/news';
 import Partners from './modules/partners';
+import RectorMessage from './modules/rector-message';
 
 const authApi = new Auth();
-const admissionApi = new Admissions()
-const partnersApi = new Partners()
-const newsApi = new News()
+const admissionApi = new Admissions();
+const partnersApi = new Partners();
+const newsApi = new News();
+const messageApi = new RectorMessage();
 
-export {
-  authApi,
-  admissionApi,
-  partnersApi,
-  newsApi
-};
+export { authApi, admissionApi, partnersApi, newsApi, messageApi };
